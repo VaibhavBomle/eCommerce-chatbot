722c3a1d5d3a11bc957d956f3fc469e845230c97
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2024-11-14 20:10:52 +0530

    Update ingest.py
    refactor ingest.py file

--- ecommbot/ingest.py
+++ ecommbot/ingest.py
@@ -1,45 +1,51 @@
 from langchain_astradb import AstraDBVectorStore
 from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from ecommbot.data_convert import dataconverter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from data_convert import dataconverter
+
+#Load env variable
+load_dotenv()
+
+# get keys from .env
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+ASTRA_DB_API_ENDPOINT=os.getenv("ASTRA_DB_API_ENDPOINT")
+ASTRA_DB_APPLICATION_TOKEN=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_KEYSPACE=os.getenv("ASTRA_DB_KEYSPACE")
 
 
-# API KEYS
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT")
-ASTRA_DB_APPLICATION_TOKEN =os.getenv("ASTRA_DB_APPLICATION_TOKEN")
-ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
+# Initializing embedding models
+embedding = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY) # OpenAI embedding
+#embedding = GoogleGenerativeAIEmbeddings(model = "models/embedding-001") # Google gemini embedding
 
-# loading embedding models
-#embedding_google = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=GEMINI_API_KEY)
-embedding_openai = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
-
-# ingest data in AstraDB
+# Ingesting Data
 def ingestdata(status):
+
+    # Here we Initializing vector db
     vstore = AstraDBVectorStore(
-        embedding=embedding_openai,
-        collection_name="chatbotecomm",
-        api_endpoint=ASTRA_DB_API_ENDPOINT,
-        TOKEN=ASTRA_DB_APPLICATION_TOKEN,
-        namespace=ASTRA_DB_KEYSPACE
-    )
-
-    storage = status
-
-    if storage == None:
-        docs = dataconverter()
+            embedding=embedding,
+            collection_name="chatbotecomm",
+            api_endpoint=ASTRA_DB_API_ENDPOINT,
+            token=ASTRA_DB_APPLICATION_TOKEN,
+            namespace=ASTRA_DB_KEYSPACE,
+        )
+    
+    storage=status
+    
+    if storage==None:
+        docs=dataconverter()
+        print("docs ==>",docs)
         inserted_ids = vstore.add_documents(docs)
     else:
         return vstore
     return vstore, inserted_ids
 
-if __name__ == "__main__":
-    vstore,inserted_ids = ingestdata(None)
-    print(f"Inserted {len(inserted_ids)} documents.")
-    results = vstore.similarity_search("can you tell me the sound basshead")
+if __name__=='__main__':
+    vstore,inserted_ids=ingestdata(None)
+    print(f"\nInserted {len(inserted_ids)} documents.")
+    results = vstore.similarity_search("can you tell me the low budget sound basshead.")
     for res in results:
-        print(f"* {res.page_content [{res.metadata}]}")
+        print(f"* {res.page_content} [{res.metadata}]")
+            
