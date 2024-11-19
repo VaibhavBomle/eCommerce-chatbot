614a3cb9ad735f9236480ea3ea37243bc7326eab
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2024-11-19 09:04:22 +0530

    Update app.py
    Updated app.py file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template,jsonify,request
 from dotenv import load_dotenv
 import os
-from ecommbot.retrieval_generation import generation
+from ecommbot.retrival_generation import generation
 from ecommbot.ingest import ingestdata
 
 app = Flask(__name__)
@@ -16,8 +16,12 @@ def index():
 
 @app.route("/get",methods = ["GET","POST"])
 def chat():
-    pass
-    
+    msg = request.form["msg"]
+    input = msg
+    result = chain.invoke(input)
+    print("Response ====>",result)
+    return str(result)
+
     
 if  __name__ == '__main__':
     app.run(debug=True)
